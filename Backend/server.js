d1620755a36c87c9ec819c332e7a288e907a5a4a
ranjanvaibhawi <ranjanvaibhawi@gmail.com>
2025-09-04 21:03:14 +0530

    Backend initial push -- User Login and Signup added, Database connected

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js"; // ✅ fixed path
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// DB connection
+mongoose.connect(process.env.MONGO_URI,{useUnifiedTopology:true, useNewUrlParser:true})
+  .then(() => console.log(" MongoDB connected"))
+  .catch((err) => console.error("MongoDB error:", err));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
